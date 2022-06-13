f44eb1946c24c96ae7fd9952d8a1115120e9c13b
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2022-06-13 21:20:04 +0700

    update template

--- KHR/src/main/java/fpt/aptech/KHR/KhrApplication.java
+++ KHR/src/main/java/fpt/aptech/KHR/KhrApplication.java
@@ -1,13 +1,20 @@
 package fpt.aptech.KHR;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KhrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KhrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KhrApplication.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
 
 }
