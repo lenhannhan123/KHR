d78976136f640a2dc3d76ce6fe15dbc0ab16d78e
Thien Nguyen <j.thien1551@gmail.com>
2022-06-16 18:28:22 +0700

    Edit pom for spring security

--- KHR/src/main/java/Securingweb/EncryptedPasswordUtils.java
+++ KHR/src/main/java/Securingweb/EncryptedPasswordUtils.java
@@ -1,19 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Securingweb;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-/**
- *
- * @author jthie
- */
-public class EncryptedPasswordUtils {
-    public static String encryptPassword(String password) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder.encode(password);
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package Securingweb;
+//
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+///**
+// *
+// * @author jthie
+// */
+//public class EncryptedPasswordUtils {
+//    public static String encryptPassword(String password) {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        return encoder.encode(password);
+//    }
+//}
--- KHR/src/main/java/Security/jwt/AuthEntryPointJwt.java
+++ KHR/src/main/java/Security/jwt/AuthEntryPointJwt.java
@@ -1,26 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Security.jwt;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-/**
- *
- * @author jthie
- */
-public class AuthEntryPointJwt implements AuthenticationEntryPoint{
-
-    @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package Security.jwt;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.AuthenticationEntryPoint;
+//
+///**
+// *
+// * @author jthie
+// */
+//public class AuthEntryPointJwt implements AuthenticationEntryPoint{
+//
+//    @Override
+//    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+//}
