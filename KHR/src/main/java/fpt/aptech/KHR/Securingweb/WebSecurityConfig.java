0977fa3fcc9eddaa3b1e298e4da022943f4144e2
Thien Nguyen <j.thien1551@gmail.com>
2022-06-17 17:04:15 +0700

    Tat authentication

--- KHR/src/main/java/fpt/aptech/KHR/Securingweb/WebSecurityConfig.java
+++ KHR/src/main/java/fpt/aptech/KHR/Securingweb/WebSecurityConfig.java
@@ -17,15 +17,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/plugins/**","/dist/**","/css/**","/images/**").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll();
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/plugins/**","/dist/**","/css/**","/images/**").permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll();
 
     }
 //
