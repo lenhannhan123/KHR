21ae0db25f23209cd4e21eb1cabdb281acd83b54
vuongpham99 <86349987+vuongpham99@users.noreply.github.com>
2022-07-11 02:16:34 +0700

    update timekeeping

--- KHR/src/main/java/fpt/aptech/KHR/Controller/TimekeepingController.java
+++ KHR/src/main/java/fpt/aptech/KHR/Controller/TimekeepingController.java
@@ -5,10 +5,12 @@
  */
 package fpt.aptech.KHR.Controller;
 
+import fpt.aptech.KHR.Entities.Account;
 import fpt.aptech.KHR.Entities.Shift;
 import fpt.aptech.KHR.Entities.Timekeeping;
 import fpt.aptech.KHR.Routes.RouteAPI;
 import fpt.aptech.KHR.Routes.RouteWeb;
+import fpt.aptech.KHR.Services.IAccountRepository;
 import fpt.aptech.KHR.Services.IShiftServices;
 import fpt.aptech.KHR.Services.ITimekeepingServices;
 import java.text.ParseException;
@@ -20,8 +22,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,17 +33,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author backs
  */
-@RestController
+@Controller
 public class TimekeepingController {
 
     @Autowired
     ITimekeepingServices timekeepingServices;
+
+    @Autowired
+    IAccountRepository accountRepository;
 
     @Autowired
     IShiftServices shiftServices;
@@ -76,15 +82,34 @@ public class TimekeepingController {
 //        shift.addAll(shiftServices.findByTime(java.sql.Time.valueOf("07:30:00"), java.sql.Time.valueOf("11:30:00")));
 //        return shift;
 //    }
-    
+    @RequestMapping(value = "timekeeping/add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("timekeeping", new Timekeeping());
+        return "timekeeping/create";
+    }
+
+    @RequestMapping(value = "timekeeping/save", method = RequestMethod.POST)
+    public ResponseEntity<String> insert(HttpServletRequest request, Timekeeping timekeeping) {
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+        String time = formatter.format(new Date());
+        //String date = formatter.format(java.sql.Time.valueOf(new Date().toString()));
+        Account account = accountRepository.findByMail(request.getParameter("mail"));
+        timekeeping = new Timekeeping();
+        timekeeping.setMail(account);
+        timekeeping.setTimestart(java.sql.Time.valueOf("07:30:00"));
+        timekeeping.setTimeend(java.sql.Time.valueOf("11:30:00"));
+        timekeepingServices.saveTimekeeping(timekeeping);
+        return new ResponseEntity<String>(time, HttpStatus.OK);
+        //timekeepingServices.saveTimekeeping(timekeeping);
+    }
+
 //    @PostMapping(value = "timekeeping/save")
-//    public ResponseEntity<Shift> insert(@RequestBody Timekeeping timekeeping) {
+//    public ResponseEntity<Timekeeping> insert(@RequestBody Timekeeping timekeeping) {
 //        try {
-//            _service.saveEmployees(employees);
-//            return new ResponseEntity<>(employees, HttpStatus.CREATED);
+//            timekeepingServices.saveTimekeeping(timekeeping);
+//            return new ResponseEntity<>(timekeeping, HttpStatus.CREATED);
 //        } catch (Exception e) {
 //            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 //        }
 //    }
-
 }
--- KHR/src/main/java/fpt/aptech/KHR/Services/ITimekeepingServices.java
+++ KHR/src/main/java/fpt/aptech/KHR/Services/ITimekeepingServices.java
@@ -14,4 +14,5 @@ import java.util.List;
  */
 public interface ITimekeepingServices {
     List<Timekeeping> findAll();
+    void saveTimekeeping(Timekeeping timekeeping);
 }
