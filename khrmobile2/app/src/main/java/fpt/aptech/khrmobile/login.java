c7731061406a68285862d5a6ef404b94156369e4
Thien Nguyen <j.thien1551@gmail.com>
2022-07-19 19:50:45 +0700

    Login Android WIP

--- khrmobile2/app/src/main/java/fpt/aptech/khrmobile/login.java
+++ khrmobile2/app/src/main/java/fpt/aptech/khrmobile/login.java
@@ -1,17 +1,19 @@
 package fpt.aptech.khrmobile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class login extends AppCompatActivity {
+    EditText Login_txtUsername;
+    EditText Login_txtPassword;
 
-
-    void openformforget(){
+    void openFormForget(){
         TextView linkforget = findViewById(R.id.Login_linkforrgetpass);
         linkforget.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,6 +27,9 @@ public class login extends AppCompatActivity {
 
     void login(){
         Button Login_button = findViewById(R.id.Login_btnlogin);
+        Login_txtPassword = findViewById(R.id.Login_txtUsername);
+        Login_txtPassword = findViewById(R.id.Login_txtPassword);
+
         Login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +44,7 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-        openformforget();
+        openFormForget();
         login();
     }
 }
