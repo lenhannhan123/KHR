a71cfb7a17ee47a05a07bd8a3d442ae870cef0cf
Thien Nguyen <j.thien1551@gmail.com>
2022-08-09 12:24:31 +0700

    IP config

--- khrmobile2/app/src/main/java/fpt/aptech/khrmobile/Config/ConfigData.java
+++ khrmobile2/app/src/main/java/fpt/aptech/khrmobile/Config/ConfigData.java
@@ -3,11 +3,9 @@ package fpt.aptech.khrmobile.Config;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import fpt.aptech.khrmobile.MainActivity;
-
 public class ConfigData {
 
-    public static String IP ="192.168.1.6";
+    public static String IP ="192.168.1.3";
     public static final String profilePreferences = "profilepref";
     public static final String Mail = "mailKey";
 
